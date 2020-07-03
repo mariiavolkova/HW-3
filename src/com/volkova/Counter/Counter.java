0ed1460a84c5e34be4a7f577c0f0ea6b196a358d
mariiavolkova <67053060+mariiavolkova@users.noreply.github.com>
2020-07-03 17:08:40 +0300

    Update Counter.java

--- src/com/volkova/Counter/Counter.java
+++ src/com/volkova/Counter/Counter.java
@@ -1,19 +1,12 @@
 package com.volkova.Counter;
 
 public class Counter {
-    public int count;
+    public static int count;
 
     public Counter() { count++;
     }
 
     public int getCount() {
-        count = this.count;
-        return count;
-    }
-
-    public void Counter() {
-        int randomNew = (int) (Math.random() * 100);
-        for (int i = 0; i < randomNew; i++) ;
-        System.out.println(randomNew);
+        return this.count;
     }
     }
